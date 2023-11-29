f58b9b51da30f65b7843ef734266624f97973a26
gigimach <112143871+gigimach@users.noreply.github.com>
2023-11-29 19:45:10 -0300

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnWoebeDRE75S4X79rE228nE5zqyeYk2Y",
-  authDomain: "chat-app-f283f.firebaseapp.com",
-  projectId: "chat-app-f283f",
-  storageBucket: "chat-app-f283f.appspot.com",
-  messagingSenderId: "859543375353",
-  appId: "1:859543375353:web:193c61a54ee65e05bb4128"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
